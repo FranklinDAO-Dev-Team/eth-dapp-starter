2eb6c713a1f27261f9c2908e05b04de3c6fa17fd
Spencer Solit <sjs@RSFAM-SJS-Pro22.local>
2023-02-04 23:24:37 -0500

    modified Auction.sol to price the auction in an ERC20. started deploy and chai testing

--- week3/contracts/Auction.sol
+++ week3/contracts/Auction.sol
@@ -13,6 +13,8 @@ contract EnglishAuction {
 
     IERC721 public immutable nft;
     uint public immutable nftId;
+    IERC20 public immutable coin;
+    
     address payable public immutable seller;
     uint32 public endAt;
     bool public started;
@@ -23,15 +25,19 @@ contract EnglishAuction {
 
 
     /* constructor TODO
-        Take in as inputs and address _nft, a unit _nftID, and a uint _startingbid
-        Initialize variables nft, nftID, and highestBid to these respective values, and seller to msg.sender
-        cast _nft as an IERC721 and seller as a payable address
+        Take in as inputs an address _nft, a uint _nftId, an address _erc20, and a uint _startingbid
+        Initialize variables nft, nftID, coin (the erc20), and highestBid to these respective values, and seller to msg.sender
+        Remember cast variables to the correct type: _nft as an IERC721, coin to IERC20, and seller as a payable address
     */
-    constructor(/*address _nft, uint _nftId, uint _startingBid*/) {
-        // nft = IERC721(_nft); 
-        // nftId = _nftId; 
-        // highestBid = _startingBid; 
-        // seller = payable(msg.sender); 
+    // constructor() {
+
+    // }
+    constructor(address _nft, uint _nftId, address _erc20, uint _startingBid) {
+        nft = IERC721(_nft); 
+        nftId = _nftId; 
+        coin = IERC20(_erc20);
+        highestBid = _startingBid;
+        seller = payable(msg.sender); 
     }
 
     function start() external {
@@ -42,23 +48,46 @@ contract EnglishAuction {
         nft.transferFrom(seller, address(this), nftId);
         emit Start();
     }
+
+    // Here we give you some functions to interact the PennCoin ERC20 contract 
+    function ApproveCoin(uint256 _coinAmount) public returns(bool){
+       coin.approve(address(this), _coinAmount);
+       return true;
+    }
+    function AcceptPayment(uint256 _tokenamount) public returns(bool) {
+       require(_tokenamount > GetAllowance(), "Please approve tokens before transferring");
+       coin.transfer(address(this), _tokenamount);
+       return true;
+    }
+    function GetUserTokenBalance() public view returns(uint256){ 
+       return coin.balanceOf(msg.sender);
+    }
+    function GetAllowance() public view returns(uint256){
+       return coin.allowance(msg.sender, address(this));
+    }
+    function GetContractTokenBalance() public view returns(uint256){
+       return coin.balanceOf(address(this));
+    }
+
     
-    function bid() external payable {
+    function bid(uint256 coinamount) external payable {
         require(started, "not started");
         require(block.timestamp < endAt, "ended");
-        require(msg.value > highestBid, " value < highest bid");
+        require(coinamount > highestBid, " value < highest bid");
+        AcceptPayment(coinamount);
         if (highestBidder != address(0)) {
             bids[highestBidder] += highestBid;
         }
-        highestBid = msg.value;
+        highestBid = coinamount;                                
         highestBidder = msg.sender;
-        emit Bid(msg.sender, msg.value);
+        emit Bid(msg.sender, coinamount);
     }
 
     function withdraw() external {
         uint bal = bids[msg.sender];
         bids[msg.sender] = 0;
-        payable(msg.sender).transfer(bal);
+        coin.approve(address(this), bal);
+        coin.transfer(msg.sender, bal);
         emit Withdraw(msg.sender, bal);
     }
 
---
+++ week3/test/AuctionTest.js
@@ -0,0 +1,38 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+
+
+beforeEach(async function () {
+  [seller, buyer1, buyer2] = await ethers.getSigners();
+  const PennCoin_Fact = await hre.ethers.getContractFactory("PennCoin");
+  const token = await PennCoin_Fact.deploy("10000000000000000000000");
+  const PennFT_Fact = await hre.ethers.getContractFactory("PennFT");
+  const PennFT = await PennFT_Fact.deploy("PennFT", "PFT");
+  const TOKEN_URI = "ipfs://QmPYXAvt8WzsmEytuSfsPrTLwqdNXPKmmF5VsJvvzPkTWW";
+  const tx = await PennFT.functions.mintNFT(wallet.getAddress(), TOKEN_URI);
+  const EnglishAuction_Fact = await hre.ethers.getContractFactory("EnglishAuction");
+  const englishAuction = await EnglishAuction_Fact.deploy(PennFT.address, PennFT_id, PennCoin.address, 5);
+});
+
+
+describe("Start the Auction", function () {    
+
+  it("", async function () {
+    await expect(
+      englishAuction.connect(buyer1).start()
+    ).to.be.reverted;
+  });
+    
+  it("Only seller can start the auction", async function () {
+    await expect(
+      englishAuction.connect(buyer1).start()
+    ).to.be.reverted;
+  });
+
+  
+});
